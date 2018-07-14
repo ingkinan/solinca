48b9fa4e5f6b46d55707a0430828f6cd13ea1999
ingkinan <kinan05gmail.com>
2018-07-14 04:33:35 -0400

    ultimos cambios antes de terminar el día

--- pagina-sofware/consulta.php
+++ pagina-sofware/consulta.php
@@ -32,7 +32,7 @@
                 <div style="height: 100%;width: 100%;display: flex;justify-content: center;align-items: center;flex-direction: column;">
                     <div style="width:60%; height:60%;display: flex;justify-content: end;align-items: center;flex-direction: column; background:#fff;">
                         <div style="width: 100%;height: 15%;display: flex;background: #41adce;color: #fff;">
-                            <h1 style="font-size: 20px;letter-spacing: 2px;padding-left: 10px;padding-top: 5px;    margin-top: 0px !important;margin-bottom: 0px !important;">CONSULTAS</h1>
+                            <h1 style="font-size: 20px;letter-spacing: 2px;padding-left: 10px;padding-top: 5px;margin-top: 0px !important;margin-bottom: 0px !important;">CONSULTAS</h1>
                         </div>
                         <div style="width: 100%;height: 100%;display: flex;justify-content: center;align-items: center;flex-direction: column;">
                             <input type="text" style="width: 50%;height: 38px;">
